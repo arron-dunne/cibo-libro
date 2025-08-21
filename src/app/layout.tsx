d6384ac2635541c2927f8d14ffdb2c9a02252fad
norra222 <arrondunne@gmail.com>
2025-08-21 14:21:24 +1000

    removed analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,5 @@
 // app/layout.tsx
 import "@/styles/globals.css";
-import { Analytics } from "@vercel/analytics/next";
-import { SpeedInsights } from "@vercel/speed-insights/next";
 import type { Metadata } from "next";
 
 export const metadata: Metadata = {
@@ -14,8 +12,6 @@ export default async function RootLayout({ children }: { children: React.ReactNo
   return (
     <html lang="en">
       <body>
-        <Analytics />
-        <SpeedInsights />
         { children}
       </body>
     </html>
