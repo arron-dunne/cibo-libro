d301531210238e39863bdfa345dcdd1b4f9c57c8
arron-dunne <arrondunneps@gmail.com>
2025-11-11 15:37:22 +1100

    layout updates

--- src/app/(main)/all/ClientRecipesGrid.tsx
+++ src/app/(main)/all/ClientRecipesGrid.tsx
@@ -248,7 +248,7 @@ export function ClientRecipesGrid({
       )}
 
       {/* Grid */}
-      <ul className="px-2 mt-4 grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-5 md:gap-6" role="list">
+      <ul className="mt-4 grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-5 md:gap-6" role="list">
         {visible.map((recipe, i) => (
           <li key={i}>
             <RecipeCard recipe={recipe as RecipeCardProps} />
--- src/app/components/recipes/RecipeCard.tsx
+++ src/app/components/recipes/RecipeCard.tsx
@@ -24,7 +24,7 @@ export function RecipeCard({ recipe }: { recipe: RecipeCardProps }) {
   const href = `/view/${recipe.slug}`
 
   return (
-    <article className="w-full aspect-[0.7] flex flex-col overflow-hidden rounded-2xl border border-zinc-200 bg-white shadow-lg transition hover:scale-105 cursor-pointer">
+    <article className="w-full aspect-square lg:aspect-[0.7] flex flex-col overflow-hidden rounded-2xl border border-zinc-200 bg-white shadow-lg transition hover:scale-105 cursor-pointer">
 
       {/* Make whole card link */}
       <Link
