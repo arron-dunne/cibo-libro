b84529f1b44e9b7c65219be9dffee9a6dc300a9b
arron-dunne <arrondunneps@gmail.com>
2025-11-11 17:59:51 +1100

    terms of service and privacy policy pages

--- src/app/components/footer/Footer.tsx
+++ src/app/components/footer/Footer.tsx
@@ -15,9 +15,9 @@ export function Footer() {
           />
         </div>
         <nav className="flex flex-wrap gap-4 text-sm">
-          <Link className="hover:underline" href="/about">About</Link>
-          <Link className="hover:underline" href="/terms">Terms of Use</Link>
-          <Link className="hover:underline" href="/privacy">Privacy Policy</Link>
+          <Link className="hover:underline" href="/legal/terms">Terms of Use</Link>
+          <Link className="hover:underline" href="/legal/privacy">Privacy Policy</Link>
+          <Link className="hover:underline" href="/support">Support</Link>
           <Link className="hover:underline" href="/contact">Contact</Link>
           <Link className="hover:underline" href="/support">Support</Link>
         </nav>
