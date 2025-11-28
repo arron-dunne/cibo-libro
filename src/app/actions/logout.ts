aff4ba8f5d88db1a8fc022a7f2f337e3c25ff02c
arron-dunne <arrondunneps@gmail.com>
2025-11-28 13:36:25 +1100

    moved logout to mobile menu

---
+++ src/app/actions/logout.ts
@@ -0,0 +1,9 @@
+"use server"
+
+import { signOut } from "@/lib/auth/auth"
+
+// server side client action for logging out
+// call this from client components
+export async function logout() {
+    await signOut();
+}
--- src/lib/auth/auth.ts
+++ src/lib/auth/auth.ts
@@ -1,4 +1,3 @@
-// src/lib/auth.ts
 import 'server-only';
 
 export const runtime = 'nodejs';
