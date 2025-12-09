384b6dc49496eb9a0c3dc9c729c361212747ca8a
arron-dunne <arrondunneps@gmail.com>
2025-12-09 12:04:21 +1100

    wip: support hub

--- src/app/components/footer/Footer.tsx
+++ src/app/components/footer/Footer.tsx
@@ -19,7 +19,6 @@ export function Footer() {
           <Link className="hover:underline" href="/terms">Terms of Use</Link>
           <Link className="hover:underline" href="/privacy">Privacy Policy</Link>
           <Link className="hover:underline" href="/contact">Contact</Link>
-          <Link className="hover:underline" href="/feedback">Feedback</Link>
         </nav>
         <div className="text-xs/6 opacity-90">© {new Date().getFullYear()} cibo libro. All rights reserved.</div>
       </div>
