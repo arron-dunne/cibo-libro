b4456ba23224e6b528d7a0b31db9175110450e0d
arron-dunne <arrondunneps@gmail.com>
2026-01-22 15:22:05 +1100

    small navbar icon tweaks

--- src/app/components/navbar/DesktopNavLink.tsx
+++ src/app/components/navbar/DesktopNavLink.tsx
@@ -13,14 +13,11 @@ export function DesktopNavLink({ type }: { type: NavLinkType }) {
   return (
     <Link
       href={href}
-      className={[
-        "flex items-center gap-1 font-medium rounded-full transition",
-        highlight
-          ? "bg-linear-to-r from-orange-500 to-rose-500 text-white px-4 py-2 font-bold shadow"
-          : "text-orange-700 hover:brightness-200",
-      ].join(" ")}
+      className={`flex items-center gap-1 font-medium rounded-full transition 
+      ${ highlight ? "bg-linear-to-r from-orange-500 to-rose-500 text-white px-4 py-2 font-bold shadow"
+        : "text-orange-700 hover:brightness-200"}`}
     >
-      {highlight && <Icon size={16} />}
+      {highlight && <Icon size={18} />}
       {label}
     </Link>
   );
