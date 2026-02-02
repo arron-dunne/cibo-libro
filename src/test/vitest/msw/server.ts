4bdcc8b7a42e8ac2dd44bbd9efdd82567cad9213
arron-dunne <arrondunneps@gmail.com>
2026-02-02 17:44:57 +1100

    detect paywall unit and integration tests

--- src/test/vitest/msw/server.ts
+++
@@ -1,9 +0,0 @@
-import { setupServer } from 'msw/node';
-import { handlers } from './handlers';
-export const server = setupServer(...handlers);
-
-// In setupTests.ts:
-import { server } from './msw/server';
-beforeAll(() => server.listen());
-afterEach(() => server.resetHandlers());
-afterAll(() => server.close());
