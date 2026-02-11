21fd0481f709a38db235bbf0f06d7597cddfbced
arron-dunne <arrondunneps@gmail.com>
2026-02-11 11:13:28 +1100

    improved ing and step layout

--- src/app/(main)/view/[slug]/page.tsx
+++ src/app/(main)/view/[slug]/page.tsx
@@ -66,7 +66,7 @@ export default async function ViewRecipePage({
       </Link>
 
       {/* Hero section */}
-      <section className="h-max md:h-120 relative mt-4 overflow-hidden rounded-4xl border border-white/70 bg-white shadow-2xl flex flex-col md:flex-row">
+      <section className="h-max md:h-120 relative mt-4 overflow-hidden rounded-4xl border border-white/60 bg-white shadow-2xl flex flex-col md:flex-row">
         {/* Source URL */}
         {recipe.sourceUrl && (
           <Link
@@ -137,14 +137,13 @@ export default async function ViewRecipePage({
 
           {/* Button bar  */}
           <div className="mt-4 flex gap-2">
-
             {/* TODO: refactor secondary button */}
             <button
               className="px-3 h-11 flex gap-2 items-center
               bg-linear-to-br from-slate-100 to-slate-200
               rounded-full text-slate-800 border border-slate-300
               cursor-pointer hover:brightness-90 active:brightness-75"
-              >
+            >
               <Heart size={20} />
               <span className="hidden lg:block">Favourite</span>
             </button>
@@ -179,60 +178,57 @@ export default async function ViewRecipePage({
       </section>
 
       {/* Steps and Ingredients */}
-      <section className="mt-8 grid items-start gap-6 md:grid-cols-[0.9fr_1.1fr]">
-        {/* Steps (left panel) */}
-        <div className="grid gap-6">
-          <Card title="Ingredients">
-            {ingredients.length ? (
-              <ul className="space-y-3">
-                {ingredients.map((it, idx) => (
-                  <li
-                    key={`ing-${idx}`}
-                    className="rounded-xl border border-orange-200/70 bg-white px-3 py-2 text-sm"
-                  >
-                    {it}
-                  </li>
-                ))}
-              </ul>
-            ) : (
-              <p className="text-sm text-slate-600">No ingredients.</p>
-            )}
-          </Card>
-
-          {/* <Card title="Notes">
-            <p className="text-sm text-slate-600">No notes yet.</p>
-          </Card>
-
-          <Card title="Serve with">
-            <p className="text-sm text-slate-600">Add sides or pairings.</p>
-          </Card> */}
-        </div>
-
-        {/* RIGHT COLUMN */}
-        <div className="grid gap-6">
-          <Card id="steps" title="Steps">
-            {steps.length ? (
-              <ol className="relative ml-3 space-y-6 before:absolute before:left-0 before:top-0 before:h-full before:w-1 before:rounded before:bg-linear-to-b before:from-orange-200 before:to-rose-200">
-                {steps.map((s, i) => (
-                  <li key={i} className="relative pl-6">
-                    <div className="absolute left-0 top-1 -translate-x-1/2 grid h-5 w-5 place-items-center rounded-full bg-orange-500 text-[11px] font-extrabold text-white shadow">
-                      {i + 1}
-                    </div>
-                    <p className="text-base leading-relaxed">{s}</p>
-                  </li>
-                ))}
-              </ol>
-            ) : (
-              <p className="text-sm text-slate-600">No steps yet.</p>
-            )}
-          </Card>
-        </div>
-      </section>
+      {recipe.type != "EXTERNAL_LINK" && (
+        <section className="mt-8 flex flex-col md:flex-row gap-8">
+          <IngredientsSection ingredients={recipe.ingredients} />
+          <StepsSection steps={recipe.steps} />
+        </section>
+      )}
     </>
   );
 }
 
-/* ====== tiny SSR helpers (no client state) ====== */
+function IngredientsSection({ ingredients }: { ingredients: string[] }) {
+  return (
+    <section className="h-max w-full md:w-1/3 lg:w-2/5 rounded-4xl border border-white/60 bg-white shadow-xl p-6">
+      <h2 className="mb-4 ml-2 text-2xl font-extrabold">Ingredients</h2>
+      {ingredients.length ? (
+        <ul className="space-y-4">
+          {ingredients.map((ing, idx) => (
+            <li key={`ing-${idx}`} className="flex gap-2">
+              <div className="h-2 w-2 mt-2 shrink-0 rounded-full bg-orange-400" />
+              {ing}
+            </li>
+          ))}
+        </ul>
+      ) : (
+        <p className="text-sm text-slate-800">No ingredients.</p>
+      )}
+    </section>
+  );
+}
+
+function StepsSection({ steps }: { steps: string[] }) {
+  return (
+    <section className="h-max w-full md:w-2/3 lg:w-3/5 rounded-4xl border border-white/60 bg-white shadow-xl p-6">
+      <h2 className="mb-4 ml-2 text-2xl font-extrabold">Steps</h2>
+      {steps.length ? (
+        <ol className="relative space-y-6 before:absolute before:left-2.5 before:top-1 before:h-[98%] before:w-1 before:rounded before:bg-linear-to-b before:from-orange-200 before:to-rose-200">
+          {steps.map((s, i) => (
+            <li key={i} className="flex gap-4">
+              <div className="h-6 w-6 z-10 mt-0.5 text-center shrink-0 rounded-full bg-orange-500 font-extrabold text-white shadow">
+                {i + 1}
+              </div>
+              <p className="text-base leading-relaxed">{s}</p>
+            </li>
+          ))}
+        </ol>
+      ) : (
+        <p className="text-sm text-slate-600">No steps yet.</p>
+      )}
+    </section>
+  );
+}
 
 function Card({
   title,
@@ -246,7 +242,7 @@ function Card({
   return (
     <section
       id={id}
-      className="overflow-hidden rounded-3xl border border-white/40 bg-white p-5 shadow-xl md:p-6"
+      className="h-max overflow-hidden rounded-3xl border border-white/40 bg-white p-5 shadow-xl md:p-6"
     >
       <div className="mb-4 flex items-center gap-2">
         <div className="h-2 w-2 rounded-full bg-orange-500" />
