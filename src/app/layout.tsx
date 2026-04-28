9004982efb3ac91baba1d077d8ea32a96c89f17e
arron-dunne <arrondunneps@gmail.com>
2026-04-28 14:23:10 +1000

    wip

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -22,21 +22,9 @@ export default async function RootLayout({
         <Analytics />
         <SpeedInsights />
 
-        <div className="min-h-screen text-gray-900 antialiased">
+        <div className="min-h-screen antialiased">
           {/* Background */}
-          <div className="fixed h-screen w-full -z-10 overscroll-none inset-0">
-            <div
-              aria-hidden
-              className="absolute w-full h-full inset-0 bg-linear-to-br from-orange-400 via-orange-500 to-rose-500"
-            />
-            <div
-              aria-hidden
-              className="fixed -top-24 -left-24 h-72 w-72 rounded-full bg-orange-200/35 blur-3xl pointer-events-none "
-            />
-            <div
-              aria-hidden
-              className="fixed inset-0 bg-linear-to-b from-transparent to-white/10 pointer-events-none"
-            />
+          <div className="fixed h-full w-full -z-10 overscroll-none inset-0 bg-[#FFEEE3]">
           </div>
 
           {children}
