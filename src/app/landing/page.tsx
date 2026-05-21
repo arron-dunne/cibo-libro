77b4e4b90cff014420016fd0fb45b0c1cd83f959
arron-dunne <arrondunneps@gmail.com>
2026-05-21 11:44:17 +1000

    landing page

--- src/app/landing/page.tsx
+++ src/app/landing/page.tsx
@@ -2,14 +2,16 @@ import Image from "next/image";
 import {
   PrimaryButton,
   SecondaryButton,
+  TeriaryButton,
 } from "@/app/components/buttons/Buttons";
-import { Header, SubHeader } from "../components/text/Headers";
-import { Calendar, Camera, GraduationCap, Icon, ShoppingBasket, UsersRound } from "lucide-react";
+import { Header, SubHeader } from "@/app/components/text/Headers";
+import { Calendar, Camera, ChevronRight, GraduationCap, ShoppingBasket, UsersRound } from "lucide-react";
+import { Footer } from "../components/footer/Footer";
+import Link from "next/link";
 
 export default function LandingPage() {
   return (
     <div>
-      {/* Hero */}
       <div className="relative w-full h-screen pt-8 px-14">
         {/* Top Bar */}
         <div className="w-full flex justify-between items-center">
@@ -20,13 +22,19 @@ export default function LandingPage() {
             height={36}
             className="h-12 w-auto"
             priority
-          />
+            />
           <div className="flex gap-4 h-max">
-            <SecondaryButton>Login</SecondaryButton>
-            <PrimaryButton>Get Started</PrimaryButton>
+            <Link href="/login">
+              <SecondaryButton>Login</SecondaryButton>
+            </Link>
+            <Link href="/register">
+              <PrimaryButton>Get Started</PrimaryButton>
+            </Link>
           </div>
         </div>
-        <div className="mt-20 w-full flex gap-12">
+
+        {/* Hero */}
+        <section id="hero" className="mt-20 w-full flex gap-12">
           <div className="my-auto space-y-8">
             <Header textSize="text-6xl">
               Your cookbook for the modern kitchen
@@ -36,8 +44,12 @@ export default function LandingPage() {
               place. Import from any website in seconds.
             </SubHeader>
             <div className="flex gap-4">
-              <PrimaryButton size="lg">Start for free</PrimaryButton>
-              <SecondaryButton size="lg">See how it works</SecondaryButton>
+              <Link href="/register">
+                <PrimaryButton size="lg">Start for free</PrimaryButton>
+              </Link>
+              <Link href="#how-it-works">
+                <SecondaryButton size="lg">See how it works</SecondaryButton>
+              </Link>
             </div>
           </div>
           <Image
@@ -47,11 +59,11 @@ export default function LandingPage() {
             height={1000}
             className="w-150 mr-20 shrink-0 rounded-[80] shadow-[0_10px_25px_rgba(255,105,0,0.25)]"
           />
-        </div>
+        </section>
       </div>
 
       {/* How it works */}
-      <div className="w-full border-t border-rose-500 min-h-screen py-20 px-14">
+      <section id="how-it-works" className="w-full border-t border-rose-500 min-h-screen py-20 px-20">
         <div className="flex flex-col items-center gap-4">
           <Header>What is CiboLibro?</Header>
           <SubHeader className="max-w-3xl text-center">
@@ -126,10 +138,10 @@ export default function LandingPage() {
             </p>
           </div>
         </div>
-      </div>
+      </section>
 
       {/* Features */}
-      <div className="w-full py-20 px-40 relative border-t border-rose-500">
+      <section id="features" className="w-full py-20 px-40 relative border-t border-rose-500">
         <Header>Loads more features are in the oven</Header>
         <SubHeader className="mt-4">
           Cibo Libro is just getting started. Join now to receive future early
@@ -167,7 +179,6 @@ export default function LandingPage() {
               className="bg-white rounded-2xl px-4 py-4 flex items-center gap-6 w-md"
               style={{ marginLeft: `calc((100% - 28rem) / 5 * ${i})` }}
             >
-              {/* <span className="w-3 h-3 rounded-full bg-linear-to-r from-orange-500 to-rose-500 shrink-0" /> */}
               {feature.Icon && (
                 <div className="rounded-xl px-2 py-2 bg-linear-to-br from-orange-500 to-rose-500 text-white">
                   <feature.Icon />
@@ -177,62 +188,56 @@ export default function LandingPage() {
             </div>
           ))}
         </div>
-        <div className="mt-4 pr-6 flex justify-end items-center gap-3 text-lg">
-          <span className="text-gray-700">What would you love to see?</span>
-          <span className="text-orange-500 font-semibold">
-            Request a feature
-          </span>
+        <div className="mt-2 w-full flex justify-end">
+          <div className="w-md px-2 flex gap-2 justify-between text-lg">
+            <SubHeader>
+              What would you love to see?
+            </SubHeader>
+            <Link href="/request">
+              <TeriaryButton>
+                Request a feature
+              </TeriaryButton>
+            </Link>
+          </div>
         </div>
         <Image
           src="/icons/dough.png"
           alt="dough"
           width={400}
           height={400}
-          className="absolute right-45 top-60 w-48 h-48 object-contain pointer-events-none"
+          className="absolute right-50 top-62 w-48 h-48 object-contain pointer-events-none"
         />
-      </div>
+        <Image
+          src="/icons/stove.png"
+          alt="dough"
+          width={400}
+          height={400}
+          className="absolute left-45 bottom-30 w-48 h-48 object-contain pointer-events-none"
+        />
+      </section>
+
       {/* CTA / Register */}
-      <div className="w-full bg-rose-50 py-24 px-14 flex flex-col items-center">
-        <h2 className={`text-4xl font-bold text-rose-600 text-center`}>
-          Ready to get cooking?
-        </h2>
-        <p className="mt-4 text-gray-500 text-center max-w-sm">
-          Save hundreds of recipes, completely free with no ads. Start your
-          personal digital cookbook now
-        </p>
-        <div className="mt-10 flex flex-col gap-6 w-full max-w-xs">
-          <div className="flex flex-col gap-2">
-            <label className="text-gray-900 text-lg">Email</label>
-            <input
-              type="email"
-              className="w-full bg-white rounded-2xl px-5 py-4 border border-rose-200"
-            />
-          </div>
-          <div className="flex flex-col gap-2">
-            <label className="text-gray-900 text-lg">Password</label>
-            <input
-              type="password"
-              className="w-full bg-white rounded-2xl px-5 py-4 border border-rose-200"
-            />
-          </div>
-          <div className="flex flex-col gap-2">
-            <label className="text-gray-900 text-lg">Confirm Password</label>
-            <input
-              type="password"
-              className="w-full bg-white rounded-2xl px-5 py-4 border border-rose-200"
-            />
-          </div>
-          <button className="w-full mt-2 bg-linear-to-r from-orange-500 to-rose-500 text-white font-bold text-lg py-4 rounded-full hover:brightness-90 active:brightness-75 transition-all">
-            Create account
-          </button>
-        </div>
-        <div className="mt-10 flex items-center gap-3 text-base">
-          <span className="text-gray-500">Still not sure how it works?</span>
-          <span className="text-rose-500 font-semibold underline underline-offset-2 cursor-pointer">
+      <section id="cta" className="w-full border-t border-rose-500 py-24 px-14 flex flex-col items-center">
+        <Header>Ready to get cooking?</Header>
+        <SubHeader className="mt-6 mb-12 max-w-3xl text-center">
+          Save and import hundreds of recipes, access them instantly, from anywhere, with no ads. Create your
+          personal digital cookbook now for free.
+        </SubHeader>
+        <Link href="register">
+          <PrimaryButton size="xl">
+            Open you personal cookbook
+            <ChevronRight size={30}/>
+          </PrimaryButton>
+        </Link>
+        <div className="mt-16 flex items-center gap-3 text-base">
+          <span className="text-slate-500">Still not sure how it works?</span>
+          <TeriaryButton>
             Explore a demo cookbook to learn more
-          </span>
+          </TeriaryButton>
         </div>
-      </div>
+      </section>
+
+      <Footer />
     </div>
   );
 }
