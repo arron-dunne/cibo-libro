7fc6675da72c27d6c04df981d326380e8b603b2b
arron-dunne <arrondunneps@gmail.com>
2026-05-28 22:29:27 +1000

    wip recipe card

--- src/app/(main)/all/ClientRecipesGrid.tsx
+++ src/app/(main)/all/ClientRecipesGrid.tsx
@@ -289,7 +289,7 @@ export function ClientRecipesGrid({
 
       {/* Grid */}
       <ul
-        className="mt-4 grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-5 md:gap-6"
+        className="mt-4 grid grid-cols-2 sm:grid-cols-3 lg:grid-cols-4 gap-4 md:gap-6"
         role="list"
       >
         {visible.map((recipe, i) => (
--- src/app/components/tags/Tags.tsx
+++ src/app/components/tags/Tags.tsx
@@ -1,12 +1,14 @@
 export function Tag({
-  children,
+  padding="px-4 py-2",
   interactive=false,
+  children,
 }: {
+  padding?: string;
   children: React.ReactNode;
   interactive?: boolean;
 }) {
   return (
-    <div className={`inline-flex items-center rounded-full bg-white text-rose-500 border border-rose-300 px-4 py-2 font-semibold text-nowrap shadow-sm shadow-rose-100 ${interactive ? "bg-linear-to-br from-white to-white hover:from-orange-50 hover:to-rose-100 hover:shadow-md active:from-orange-100 active:to-rose-300 cursor-pointer" : ""}`}>
+    <div className={`${padding} inline-flex items-center rounded-full bg-white text-rose-500 border border-rose-300 font-semibold text-nowrap shadow-sm shadow-rose-100 ${interactive ? "bg-linear-to-br from-white to-white hover:from-orange-50 hover:to-rose-100 hover:shadow-md active:from-orange-100 active:to-rose-300 cursor-pointer" : ""}`}>
       {children}
     </div>
   );
