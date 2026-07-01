1825231a53dd80aab4c212f5071af901eb76d2fb
arron-dunne <arrondunneps@gmail.com>
2026-07-01 13:29:33 +1000

    hide navbar and footer on cookmode

--- src/app/components/footer/Footer.tsx
+++ src/app/components/footer/Footer.tsx
@@ -1,7 +1,18 @@
+"use client";
+
 import Image from "next/image";
 import Link from "next/link";
+import { usePathname } from "next/navigation";
 
 export function Footer() {
+
+  const pathname = usePathname();
+
+  // No footer on cook mode
+  if (/^\/cook\//.test(pathname)) {
+    return <></>
+  }
+  
   return (
     <footer className="mt-12 sm:mt-18 border-t border-white/80 bg-white/50 px-12 py-4 text-slate-600 backdrop-blur-xl flex flex-col sm:flex-row gap-4 items-center justify-between">
         <div className="flex-1">
